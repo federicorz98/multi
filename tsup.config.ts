7135330fecaf1d37209085db5f93f71cb13227de
Federico <fruiz+github@estudiogallo.com.ar>
2024-11-04 23:56:08 -0300

    fix

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts',
+  },
+  external: [],
+  format: ['esm', 'cjs'],
+  esbuildPlugins: [],
+  dts: 'src/index.ts',
+});
